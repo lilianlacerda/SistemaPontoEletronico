9b12c5643bb8ef5ef49e10ac2fed66627a9e8b19
lilianlacerda <lilianraiany@gmail.com>
2025-03-27 22:15:02 -0300

    nomear corretamente o nome dos botoes

--- Views/TelaRegistroPonto.Designer.cs
+++ Views/TelaRegistroPonto.Designer.cs
@@ -29,58 +29,58 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnEntrada = new System.Windows.Forms.Button();
+            this.btnSaida = new System.Windows.Forms.Button();
+            this.lblHoraAtual = new System.Windows.Forms.Label();
+            this.lblTempoTrabalhado = new System.Windows.Forms.Label();
+            this.horaAtual = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // button1
+            // btnEntrada
             // 
-            this.button1.Location = new System.Drawing.Point(263, 160);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnEntrada.Location = new System.Drawing.Point(131, 128);
+            this.btnEntrada.Name = "btnEntrada";
+            this.btnEntrada.Size = new System.Drawing.Size(110, 34);
+            this.btnEntrada.TabIndex = 0;
+            this.btnEntrada.Text = "Entrada";
+            this.btnEntrada.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnSaida
             // 
-            this.button2.Location = new System.Drawing.Point(424, 160);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnSaida.Location = new System.Drawing.Point(284, 128);
+            this.btnSaida.Name = "btnSaida";
+            this.btnSaida.Size = new System.Drawing.Size(112, 34);
+            this.btnSaida.TabIndex = 1;
+            this.btnSaida.Text = "Saída";
+            this.btnSaida.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // lblHoraAtual
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(302, 86);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.lblHoraAtual.AutoSize = true;
+            this.lblHoraAtual.Location = new System.Drawing.Point(141, 246);
+            this.lblHoraAtual.Name = "lblHoraAtual";
+            this.lblHoraAtual.Size = new System.Drawing.Size(57, 13);
+            this.lblHoraAtual.TabIndex = 2;
+            this.lblHoraAtual.Text = "Hora Atual";
             // 
-            // label2
+            // lblTempoTrabalhado
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(421, 86);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.lblTempoTrabalhado.AutoSize = true;
+            this.lblTempoTrabalhado.Location = new System.Drawing.Point(290, 246);
+            this.lblTempoTrabalhado.Name = "lblTempoTrabalhado";
+            this.lblTempoTrabalhado.Size = new System.Drawing.Size(97, 13);
+            this.lblTempoTrabalhado.TabIndex = 3;
+            this.lblTempoTrabalhado.Text = "Tempo Trabalhado";
             // 
             // TelaRegistroPonto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(553, 413);
+            this.Controls.Add(this.lblTempoTrabalhado);
+            this.Controls.Add(this.lblHoraAtual);
+            this.Controls.Add(this.btnSaida);
+            this.Controls.Add(this.btnEntrada);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "TelaRegistroPonto";
@@ -94,10 +94,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnEntrada;
+        private System.Windows.Forms.Button btnSaida;
+        private System.Windows.Forms.Label lblHoraAtual;
+        private System.Windows.Forms.Label lblTempoTrabalhado;
+        private System.Windows.Forms.Timer horaAtual;
     }
 }
